05a48f357b530a4298aa97cf1b6815208909f8be
Nkechi Anyanwu <kechie.94@gmail.com>
2024-04-10 19:08:24 +0100

    Linting

--- database/config/config.js
+++ database/config/config.js
@@ -1,31 +1,32 @@
+/* eslint-disable no-undef */
 require('dotenv').config();
 const pg = require('pg'); 
 
 console.log('process.env: ', process.env);
 
 module.exports = {
-  development: {
-    username: process.env.POSTGRES_DEV_USER,
-    password: process.env.POSTGRES_DEV_PASSWORD,
-    database: process.env.POSTGRES_DEV_DB_NAME,
-    host: process.env.POSTGRES_DEV_HOST,
-    dialect: 'postgres',
-    dialectModule: pg,
-  },
-  test: {
-    username: process.env.POSTGRES_TEST_USER,
-    password: process.env.POSTGRES_TEST_PASSWORD,
-    database: process.env.POSTGRES_TEST_DB_NAME,
-    host: process.env.POSTGRES_TEST_HOST,
-    dialect: 'postgres',
-    dialectModule: pg,
-  },
-  production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB_NAME,
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-    dialectModule: pg,
-  }
-}
+    development: {
+        username: process.env.POSTGRES_DEV_USER,
+        password: process.env.POSTGRES_DEV_PASSWORD,
+        database: process.env.POSTGRES_DEV_DB_NAME,
+        host: process.env.POSTGRES_DEV_HOST,
+        dialect: 'postgres',
+        dialectModule: pg,
+    },
+    test: {
+        username: process.env.POSTGRES_TEST_USER,
+        password: process.env.POSTGRES_TEST_PASSWORD,
+        database: process.env.POSTGRES_TEST_DB_NAME,
+        host: process.env.POSTGRES_TEST_HOST,
+        dialect: 'postgres',
+        dialectModule: pg,
+    },
+    production: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB_NAME,
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres',
+        dialectModule: pg,
+    }
+};
